d3e1120b027ad7bd1c1d3986407afd78dea1eba3
IzimDev <131653827+IzimDev@users.noreply.github.com>
2023-07-28 16:27:30 +0200

    Add files via upload

---
+++ Password_Generator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string capitalLetters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string smallLetters = "qwertyuiopasdfghjklzxcvbnm";
+            string digits = "0123456789";
+            string specialCharacters = "!@#$%^&*()-_=+<,>.";
+            string allChars = capitalLetters + smallLetters + digits + specialCharacters;
+
+            string password = "";
+
+            Console.WriteLine("Enter the password length: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < length; i++)
+            {
+                int randomValue = RandomNumberGenerator.GetInt32(0, allChars.Length);
+                char character = allChars[randomValue];
+                password += character;
+            }
+
+            Console.WriteLine(password);
+        }
+    }
+}
